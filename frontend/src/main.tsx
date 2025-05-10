2024c9fc3d85b7a1d348e8ab2a63bb831f35d9d5
Thibault Souris <stranxik@gmail.com>
2025-05-10 23:03:18 +0200

    fixe : router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,12 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Enable v7 relative splat path behavior to resolve React Router warning
+// @ts-ignore
+window.REACT_ROUTER_FUTURE_FLAGS = window.REACT_ROUTER_FUTURE_FLAGS || {};
+// @ts-ignore
+window.REACT_ROUTER_FUTURE_FLAGS.v7_relativeSplatPath = true;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
